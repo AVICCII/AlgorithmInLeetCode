12dd3e71b14fb98f0b9aff5b119ec3f321dd3b2f
AVICCII <863134471@qq.com>
2020-09-04 17:04:24 +0800

    2020/9/4

--- src/easy/Case13reverse.java
+++ src/easy/Case13reverse.java
@@ -15,13 +15,10 @@ public class Case13reverse {
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-<<<<<<< HEAD
             if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-=======
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
->>>>>>> origin/master
             rev = rev * 10 + pop;
         }
         return rev;
