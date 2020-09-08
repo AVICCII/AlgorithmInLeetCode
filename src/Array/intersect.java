3838c4e2df61f4967ed3efa51c5d7fbcc339c565
AVICCII <863134471@qq.com>
2020-09-08 18:06:38 +0800

    2020/9/8

---
+++ src/Array/intersect.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.*;
+
+/**
+ * @author aviccii 2020/9/8
+ * @Discrimination 给定两个数组，编写一个函数来计算它们的交集。
+ */
+public class intersect {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+}
