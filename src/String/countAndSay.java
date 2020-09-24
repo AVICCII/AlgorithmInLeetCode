bf5400fd3a10bc752c3e4a9c880bbcbe7f6b7fee
AVICCII <863134471@qq.com>
2020-09-24 16:53:45 +0800

    2020/09/24

---
+++ src/String/countAndSay.java
@@ -0,0 +1,11 @@
+package String;
+
+/**
+ * @author aviccii 2020/9/24
+ * @Discrimination 给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。
+ * 注意：整数序列中的每一项将表示为一个字符串。
+ *
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ */
+public class countAndSay {
+}
---
+++ src/String/strStr.java
@@ -0,0 +1,19 @@
+package String;
+
+/**
+ * @author aviccii 2020/9/24
+ * @Discrimination 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ */
+public class strStr {
+    public int strStr(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+
+        for (int start = 0; start < n - L + 1; ++start) {
+            if (haystack.substring(start, start + L).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+}
