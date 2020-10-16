7fae7b1e37246afad1f47d127908a7d93cf38467
AVICCII <863134471@qq.com>
2020-10-16 16:33:45 +0800

    2020/10/16

--- src/SGGAlogrithmDS/queue/ArrayQueueDemo.java
+++ src/SGGAlogrithmDS/queue/ArrayQueueDemo.java
@@ -3,6 +3,8 @@ package SGGAlogrithmDS.queue;
 import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 
 import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author aviccii 2020/10/15
@@ -10,7 +12,52 @@ import java.lang.reflect.Array;
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
-
+        //测试
+        //创建一个队列
+        ArrayQueue arrayQueue = new ArrayQueue(30);
+        char key = ' ';//接受用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop){
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列里取出数据");
+            System.out.println("h(head):显示队列头的数据");
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop=false;
+                    break;
+                case 'a':
+                    System.out.println("输入一个数");
+                    int value = scanner.nextInt();
+                    arrayQueue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int queue = arrayQueue.getQueue();
+                        System.out.printf("取出的数据是%d\n",queue);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int queue = arrayQueue.headQueue();
+                        System.out.printf("队列头的数据是%d\n",queue);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+            }
+            System.out.println("程序退出");
+        }
     }
 }
 
