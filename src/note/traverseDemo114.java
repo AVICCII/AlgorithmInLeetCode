7156dc26703d6c7daac2e1110f2571c0c7bc9887
AVICCII <863134471@qq.com>
2020-11-04 13:48:09 +0800

    2020/11/4

---
+++ src/note/traverseDemo114.java
@@ -0,0 +1,52 @@
+package note;
+
+/**
+ * @author aviccii 2020/11/4
+ * @Discrimination
+ */
+public class traverseDemo114 {
+
+    public void flatten(TreeNode root){
+        // base
+        if (root==null){
+            return;
+        }
+
+        //前提：左右子树已被拉平。这里进行处理
+        flatten(root.left);
+        flatten(root.right);
+
+        //后序遍历
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        //将左子树作为右子树
+        root.left=null;
+        root.right=left;
+
+        //将原先的右子树接到当前右子树的末端
+        while (root.right != null){
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
+
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
