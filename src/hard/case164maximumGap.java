2ef099d4cf7d0b6f0385d35521d828311c452912
AVICCII <863134471@qq.com>
2020-11-26 18:06:28 +0800

    2020/11/26

---
+++ src/hard/case164maximumGap.java
@@ -0,0 +1,58 @@
+package hard;
+
+import java.util.Arrays;
+
+/**
+ * @author aviccii 2020/11/26
+ * @Discrimination 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+ * <p>
+ * 如果数组元素个数小于 2，则返回 0。
+ */
+//cheat
+public class case164maximumGap {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) return 0;
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] > max) max = nums[i] - nums[i - 1];
+        }
+        return max;
+    }
+
+    //基数排序/桶排序处理
+    public int maximumGap2(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        long exp = 1;
+        int[] buf = new int[n];
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+
+        while (maxVal >= exp) {
+            int[] cnt = new int[10];
+            for (int i = 0; i < n; i++) {
+                int digit = (nums[i] / (int) exp) % 10;
+                cnt[digit]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / (int) exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = Math.max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }
+
+}
