42cff9892afda08b8e03b4d49ab71e3644ad7821
AVICCII <863134471@qq.com>
2020-11-27 18:04:24 +0800

    2020/11/27

---
+++ src/normal/case454fourSumCount.java
@@ -0,0 +1,39 @@
+package normal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author aviccii 2020/11/27
+ * @Discrimination 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ *
+ * 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+ *
+ * 例如:输入:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ *
+ * 输出:
+ * 2
+ */
+public class case454fourSumCount {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : A) {
+            for (int v : B) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : C) {
+            for (int v : D) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
