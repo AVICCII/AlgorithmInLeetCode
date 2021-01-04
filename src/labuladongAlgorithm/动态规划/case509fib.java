4d70d3cce2be269fdce07f16213f5c4cf59c01d9
AVICCII <863134471@qq.com>
2021-01-04 16:05:37 +0800

    2021/1/4

---
+++ src/labuladongAlgorithm/动态规划/case509fib.java
@@ -0,0 +1,34 @@
+package labuladongAlgorithm.动态规划;
+
+/**
+ * @author aviccii 2021/1/4
+ * @Discrimination
+ */
+public class case509fib {
+
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+
+    public static int fib(int n) {
+        return recursion(n);
+    }
+    public static int recursion(int n){
+        if (n == 1) return 1;
+        if (n == 0) return 0;
+        return recursion(n-1)+recursion(n-2);
+    }
+    //DP
+    public int fib2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
